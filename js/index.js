26c5984f101ade02e96d775d45406c0c32051569
William Bueno <165913163+uiubueno@users.noreply.github.com>
2024-12-31 18:28:22 -0300

    Add files via upload
    Adicionado estrutura inicial do projeto com HTML e CSS. Layout básico criado, aguardando implementação do TypeScript.

---
+++ js/index.js
@@ -0,0 +1,4 @@
+"use strict";
+const form = document.querySelector("#search-form > form");
+const input = document.querySelector("#input-localization");
+form === null || form === void 0 ? void 0 : form.addEventListener("submit", (event) => { });
---
+++ ts/index.ts
@@ -0,0 +1,4 @@
+const form = document.querySelector("#search-form > form");
+const input = document.querySelector("#input-localization");
+
+form?.addEventListener("submit", (event) => {});
